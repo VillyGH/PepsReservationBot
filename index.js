c8c3ca4df37339988c484142bb08fb3b90399b5a
William Blanchet Lafrenière <willclash@outlook.com>
2024-03-19 14:48:16 -0400

    refactoring + config

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import puppeteer from "puppeteer";
-import { delay, formatText } from "./util.js";
 import config from "./config.json" assert {type: "json"};
-
 
 
 (async () => {
@@ -54,7 +52,7 @@ async function sportsPage(page) {
     console.log("Loading sports page");
     let selector = `a[href='/rtpeps/Reservation/Disponibilites?selectedActivite=${config.sport}']`;
     await page.waitForSelector(selector);
-    await page.screenshot({ path: "screenshots/SportsPage.jpg" });
+    await page.screenshot({path: "screenshots/SportsPage.jpg"});
     try {
         await page.click(selector);
     } catch (e) {
@@ -67,7 +65,7 @@ async function schedulePage(page) {
     console.log("Loading schedule page");
     let selector = 'tr:not(tr[style="display:none;"]):not(.strong)';
     await page.waitForSelector(selector);
-    await page.screenshot({ path: "screenshots/SchedulePage.jpg" });
+    await page.screenshot({path: "screenshots/SchedulePage.jpg"});
     const data = await page.$$eval(selector, rows => {
         return Array.from(rows, row => {
             const columns = row.querySelectorAll('td');
@@ -80,7 +78,7 @@ async function schedulePage(page) {
         });
     });
     let found = false;
-    for (let i = 0;i < data.length; i++) {
+    for (let i = 0; i < data.length; i++) {
         if (data[i].time === config.date.time) {
             console.log(`Found ${data[i].location} Terrain ${data[i].terrain}`);
             await page.click(`a[href='${data[i].btnHref}']`, true);
@@ -88,7 +86,7 @@ async function schedulePage(page) {
             break;
         }
     }
-    if(!found) {
+    if (!found) {
         console.log("The specified reservation hour is not available for that specific date");
         process.exit(1);
     }
@@ -99,18 +97,18 @@ async function reservationPage(page) {
     console.log("Loading reservation page");
     let selector = '#radioRaquette2';
     await page.waitForSelector(selector);
-    await selectPartner(page, 0, config.ni_partenaire1);
-    if(config.ni_partenaire2 && config.ni_partenaire3) {
+    await selectPartner(page, 0, config.partner_ni1);
+    if (config.partner_ni2 && config.partner_ni3) {
         await page.click(selector);
-        await selectPartner(page, 1, config.ni_partenaire2);
-        await selectPartner(page, 2, config.ni_partenaire3);
+        await selectPartner(page, 1, config.partner_ni2);
+        await selectPartner(page, 2, config.partner_ni3);
     }
     await page.click('input[type="submit"]');
     
-    await page.screenshot({ path: "screenshots/ReservationPage.jpg" });
+    await page.screenshot({path: "screenshots/ReservationPage.jpg"});
 }
 
-async function selectPartner(page, partnerId, partnerNI){
+async function selectPartner(page, partnerId, partnerNI) {
     let selector = `select[name="ddlPartenaire${partnerId}"]`;
     const selectElement = await page.waitForSelector(selector);
     const options = await selectElement.$$eval('option', options => {
