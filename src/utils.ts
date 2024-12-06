0504eb36dc654ab83585081a3397aba681f2bdd9
VillyGH <willclash@outlook.com>
2024-12-06 01:18:08 -0500

    added logger in utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,11 @@
 import {Page} from "puppeteer";
 import {ScheduleRows} from "./types";
+import {infoLogger} from "./loggerTypes";
 
 
 export async function click(page : Page, selector : any) : Promise<void> {
     await page.waitForSelector(selector);
-    await page.evaluate((selector : any) : void => {
-        document.querySelector(selector).click();
-    }, selector);
+    await page.$eval(selector, (element) => element.click());
 }
 
 export function timeToMs(timeString : string) : number {
@@ -17,10 +16,10 @@ export function timeToMs(timeString : string) : number {
 export function findRowIndexWithTime(time : string, data : ScheduleRows[]) : number {
     for (let i : number = 0; i < data.length; i++) {
         if (data[i].time === time) {
-            console.log(`Found ${data[i].location} Terrain ${data[i].terrain}`);
+            infoLogger.success(`Found ${data[i].location} Terrain ${data[i].terrain}`);
             return i;
         }
     }
-    console.log("The specified reservation hour is not available for that specific date");
+    infoLogger.error("The specified reservation hour is not available for that specific date");
     process.exit(1);
 }
